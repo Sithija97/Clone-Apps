bbcec839d3f742d3bf68a528893e548d858d50b1
Sithija Shehara <nsithijashehara@gmail.com>
2021-02-23 23:52:44 +0530

    Added Chat Component

--- slack/src/components/Sidebar/Sidebar.js
+++ slack/src/components/Sidebar/Sidebar.js
@@ -2,8 +2,17 @@ import React, { useState, useEffect } from 'react'
 import './Sidebar.css'
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
-import SidebarOptions from './SidebarOptions';
 import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
+import SidebarOptions from './SidebarOptions';
 import db from '../firebase'
 
 function Sidebar() {
@@ -29,6 +38,15 @@ function Sidebar() {
                 <CreateIcon/>
             </div>
             <SidebarOptions Icon={InsertCommentIcon} title='Threads'/>
+            <SidebarOptions Icon={InboxIcon} title='Mensions & reactions'/>
+            <SidebarOptions Icon={DraftsIcon} title='Saved Items'/>
+            <SidebarOptions Icon={BookmarkBorderIcon} title='Channel browser'/>
+            <SidebarOptions Icon={PeopleAltIcon} title='People & user groups'/>
+            <SidebarOptions Icon={AppsIcon} title='Apps'/>
+            <SidebarOptions Icon={FileCopyIcon} title='File browser'/>
+            <SidebarOptions Icon={ExpandLessIcon} title='Show less'/>
+            <SidebarOptions Icon={ExpandMoreIcon} title='Channels'/>
+            <SidebarOptions Icon={AddIcon} title='Add Channel'/>
             {channels.map(channel => (
                  <SidebarOptions title={channel.name} id={channel.id} />
             ))}
