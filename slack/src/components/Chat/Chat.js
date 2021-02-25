c5cba8cd9c441726eb9fb37392b9b23e3f8990b7
Sithija Shehara <nsithijashehara@gmail.com>
2021-02-25 23:45:58 +0530

    URL changes according to the chanel

--- slack/src/components/Chat/Chat.js
+++ slack/src/components/Chat/Chat.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 import './Chat.css'
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 
 function Chat() {
+    const { roomId } = useParams()
     return (
         <div className='chat'>
-            
+            <div className="chat_header">
+                <div className="chat_headerLeft">
+                    <h4 className="chat_channelName">
+                        <strong>#general</strong>
+                        <StarBorderIcon />
+                    </h4>
+                </div>
+                <div className="chat_headerRight">
+                    <p><InfoOutlinedIcon /> Details</p>
+                </div>
+            </div>
         </div>
     )
 }
--- slack/src/components/Sidebar/SidebarOptions.js
+++ slack/src/components/Sidebar/SidebarOptions.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import './SidebarOptions.css'
+import { useHistory } from 'react-router-dom';
 import db from '../firebase'
 
-function SidebarOptions({Icon, title}) {
+function SidebarOptions({ Icon, title, id, addChannelOption }) {
+    const history = useHistory()
+    const selectChannel = () => {
+        if (id) {
+            history.push(`/room/${id}`)
+        } else {
+            history.push(`title`)
+        }
+    }
+    const addChannel = () => {}
     return (
-        <div className='sidebarOption'>
-            {Icon && <Icon className='sidebarOption_icon'/>}
+        <div className='sidebarOption' onClick={addChannelOption ? addChannel : selectChannel }>
+            {Icon && <Icon className='sidebarOption_icon' />}
             {Icon ? <h3>{title}</h3> : <h3 className='sidebarOption_channel'><span className='sidebarOption_hash'># </span>{title}</h3>}
-            {console.log('inside options'+title)}
+            {console.log('inside options' + title)}
         </div>
     )
 }
 
-export default  SidebarOptions;
+export default SidebarOptions;
