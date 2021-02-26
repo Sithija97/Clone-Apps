c9e55676743a676fb0fa90aa4eed7e7238c902d6
Sithija Shehara <nsithijashehara@gmail.com>
2021-02-26 12:22:10 +0530

    Heading title chanes according to the channel name

--- slack/src/components/Sidebar/SidebarOptions.js
+++ slack/src/components/Sidebar/SidebarOptions.js
@@ -25,7 +25,6 @@ function SidebarOptions({ Icon, title, id, addChannelOption }) {
         <div className='sidebarOption' onClick={addChannelOption ? addChannel : selectChannel }>
             {Icon && <Icon className='sidebarOption_icon' />}
             {Icon ? <h3>{title}</h3> : <h3 className='sidebarOption_channel'><span className='sidebarOption_hash'># </span>{title}</h3>}
-            {console.log('inside options' + title)}
         </div>
     )
 }
