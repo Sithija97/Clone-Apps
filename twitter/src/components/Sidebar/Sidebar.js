e3d26ebb0b421d699ba453a7b2feb9b21a8b0d13
Sithija Shehara <nsithijashehara@gmail.com>
2021-03-01 22:49:44 +0530

    Sidebar UI Improvements

--- twitter/src/components/Sidebar/Sidebar.js
+++ twitter/src/components/Sidebar/Sidebar.js
@@ -9,18 +9,21 @@ import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 import ListAltIcon from '@material-ui/icons/ListAlt';
 import PersonIcon from '@material-ui/icons/Person';
 import SidebarOption from './SidebarOption';
+import { Button } from '@material-ui/core';
 
 function Sidebar() {
     return (
         <div>
             <TwitterIcon/>
-            <SidebarOption Icon={HomeIcon} text='Home'/>
+            <SidebarOption active Icon={HomeIcon} text='Home'/>
             <SidebarOption Icon={SearchIcon} text='Explore'/>
             <SidebarOption Icon={NotificationsActiveIcon} text='Notifications'/>
             <SidebarOption Icon={MessageIcon} text='Messages'/>
             <SidebarOption Icon={BookmarkBorderIcon} text='Bookmarks'/>
             <SidebarOption Icon={ListAltIcon} text='Lists'/>
             <SidebarOption Icon={PersonIcon} text='Profile'/>
+
+            <Button variant='outlined' className='sidebar_tweet' fullWidth>Tweet</Button>
         </div>
     )
 }
