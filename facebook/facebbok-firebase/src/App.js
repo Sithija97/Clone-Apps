a5a3530318d39f4ec8ecd6984410d9e846e641a3
Sithija Shehara <nsithijashehara@gmail.com>
2021-04-21 13:39:25 +0530

    Header Bar UI completed

--- facebook/facebbok-firebase/src/App.js
+++ facebook/facebbok-firebase/src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="app">
-      <h1>Facebook </h1>
-    </div>
+    <Header/>
   );
 }
 
