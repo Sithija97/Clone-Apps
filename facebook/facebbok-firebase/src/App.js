7b4eee317cb95029b49947fccad554d938bf9ee8
Sithija Shehara <nsithijashehara@gmail.com>
2021-04-22 00:07:23 +0530

    Story Reel UI

--- facebook/facebbok-firebase/src/App.js
+++ facebook/facebbok-firebase/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Feed from './components/Feed/Feed';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <div className="app_body">
           <Sidebar/>
+          <Feed/>
       </div>
     </div>
   );
